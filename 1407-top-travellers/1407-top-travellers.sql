d3a5558799c3342dc852d23677cb84238a192fa3
Mufaro Matura <maturamufaro@gmail.com>
2022-09-07 08:15:52 +0200

    Time: 1099 ms (38.84%), Space: 0B (100.00%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT Users.name, COALESCE(SUM(distance),0) travelled_distance
+FROM Users
+LEFT JOIN Rides
+ON Users.id = Rides.user_id
+GROUP BY Users.id
+ORDER BY travelled_distance DESC, Users.name ASC
